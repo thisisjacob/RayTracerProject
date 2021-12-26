4c9c06be71b7ba062bf999d910fe5266a1260b5a
thisisjacob <jacobholfelder@gmail.com>
2021-12-26 12:06:06 -0500

    Documentation updated

--- RayTracer/WorldState.cpp
+++ RayTracer/WorldState.cpp
@@ -3,20 +3,20 @@
 WorldState::WorldState() {
 }
 
-bool WorldState::AddSurface(std::shared_ptr<Surface> surface) {
+bool WorldState::AddSurface(const std::shared_ptr<Surface> surface) {
 	surfaces.push_back(surface);
 	return true;
 }
 
-bool WorldState::AddLight(std::shared_ptr<Light> light) {
+bool WorldState::AddLight(const std::shared_ptr<Light> light) {
 	lights.push_back(light);
 	return true;
 }
 
-std::vector<std::shared_ptr<Surface>> WorldState::GetSurfaces() {
+const std::vector<std::shared_ptr<Surface>> WorldState::GetSurfaces() {
 	return surfaces;
 }
 
-std::vector<std::shared_ptr<Light>> WorldState::GetLights() {
+const std::vector<std::shared_ptr<Light>> WorldState::GetLights() {
 	return lights;
 }
--- RayTracer/WorldState.h
+++ RayTracer/WorldState.h
@@ -4,14 +4,16 @@
 #include <vector>
 
 // Holds all of the surfaces and lights to be used by the Ray Tracer
+// This should be used whenever information about the world is needed
 class WorldState {
 private:
 	std::vector<std::shared_ptr<Surface>> surfaces;
 	std::vector<std::shared_ptr<Light>> lights;
 public:
 	WorldState();
+	// After these objects have been added to WorldState, they should not be modified outside of WorldState
 	bool AddSurface(std::shared_ptr<Surface> surface);
 	bool AddLight(std::shared_ptr<Light> light);
-	std::vector<std::shared_ptr<Surface>> GetSurfaces();
-	std::vector<std::shared_ptr<Light>> GetLights();
+	const std::vector<std::shared_ptr<Surface>> GetSurfaces();
+	const std::vector<std::shared_ptr<Light>> GetLights();
 };
