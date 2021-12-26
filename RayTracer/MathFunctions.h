ce56362413091a61ad2053330d57850e76b46de8
thisisjacob <jacobholfelder@gmail.com>
2021-12-26 12:25:54 -0500

    Removed Normalize function

--- RayTracer/MathFunctions.h
+++ RayTracer/MathFunctions.h
@@ -10,8 +10,3 @@ T Clamp(T val, T min, T max) {
 	else
 		return max;
 }
-
-// TODO: Currently not functioning as expected
-int Normalize(int val, int maxOld, int maxNew) {
-	return (int)(maxNew * (val / (float)(maxOld)));
-}
