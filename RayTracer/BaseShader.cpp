86a4dd9d27a64568cad162b679693cbb7266c085
thisisjacob <jacobholfelder@gmail.com>
2022-01-03 20:53:46 -0500

    Initial work on shader. Graphical problems must be solved

--- RayTracer/BaseShader.cpp
+++ RayTracer/BaseShader.cpp
@@ -11,25 +11,37 @@ BlinnPhong::BlinnPhong() {
 
 glm::tvec3<double> BlinnPhong::Shading(HitData& hitData, WorldState& world) {
 	glm::tvec3<double> unitNormal = hitData.HitSurface.get()->GetUnitSurfaceNormal(hitData);
-	glm::tvec3<double> intersection = hitData.HitSurface.get()->GetIntersectionPoint(hitData);
+	glm::tvec3<double> surfacePoint = hitData.HitSurface.get()->GetIntersectionPoint(hitData);
 	glm::tvec3<double> lightCalc = glm::tvec3<double>(0.0);
-	// Calculate lighting based on every light source in world
+	// Ambient calculation, always exists
+	lightCalc += ambientCoeff * ambientIntensity;
+	// Calculate lighting
 	for (auto light : world.GetLights()) {
 		glm::tvec3<double> intensity = light.get()->lightIntensity;
-		glm::tvec3<double> lVecUnit = light.get()->lightPos - intersection;
+		glm::tvec3<double> lVecUnit = light.get()->lightPos - surfacePoint;
 		lVecUnit = glm::normalize(lVecUnit);
-		// Calculate diffuse lighting
-		lightCalc += intensity * diffuseCoeff *  std::max(0.0, glm::dot(unitNormal, lVecUnit));
-		// Calculate specular lighting
-		glm::tvec3<double> vVecUnit = world.GetCamera().GetEye() - intersection;
-		vVecUnit = glm::normalize(vVecUnit);
-		glm::tvec3<double> h = vVecUnit + lVecUnit;
-		h /= (vVecUnit + lVecUnit).length();
-		h = glm::normalize(h);
-		lightCalc += specularCoeff * intensity * std::pow(std::max(0.0, glm::dot(unitNormal, h)), phongExponent);
+		HitData shadowRecord;
+		shadowRecord.IsHit = false;
+		shadowRecord.T = std::numeric_limits<double>::infinity();
+		// Check for objects blocking light source
+		for (auto surface : world.GetSurfaces()) {
+			Ray shadowRay = Ray(surfacePoint, -lVecUnit.z, lVecUnit.x, lVecUnit.y);
+			surface->IsHit(shadowRay, 0.0000000000001, std::numeric_limits<double>::infinity(), shadowRecord);
+			
+		}
+		// If light source is blocked at pixel, do not calculate effect of that light source on the pixel
+		if (!shadowRecord.IsHit) {
+			// Calculate diffuse lighting
+			lightCalc += intensity * diffuseCoeff * std::max(0.0, glm::dot(unitNormal, lVecUnit));
+			// Calculate specular lighting
+			glm::tvec3<double> vVecUnit = world.GetCamera().GetEye() - surfacePoint;
+			vVecUnit = glm::normalize(vVecUnit);
+			glm::tvec3<double> h = vVecUnit + lVecUnit;
+			h /= (vVecUnit + lVecUnit).length();
+			h = glm::normalize(h);
+			lightCalc += specularCoeff * intensity * std::pow(std::max(0.0, glm::dot(unitNormal, h)), phongExponent);
+		}
 	}
-	// ambient calculation
-	lightCalc += ambientCoeff * ambientIntensity;
 
 	return glm::tvec3<double>(Clamp(lightCalc.x, 0.0, 1.0), Clamp(lightCalc.y, 0.0, 1.0), Clamp(lightCalc.z, 0.0, 1.0));
 }
